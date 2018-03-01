7c684c9e2473aa13ca4776f0b725f9a7ab7056bd
ajay <aajay0288@gmail.com>
2018-03-02 00:17:34 +0530

    first

--- Django Projects/first/firstapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Webpage, AccessRecord,Userprofile
-
-admin.site.register(Userprofile)
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(AccessRecord)
--- Django Projects/first/firstapp/validators.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-def startswitha(value):
-    if value[0] != 'a':
-        raise forms.ValidationError("please start a")
-    return str(value)
-
-
